c7e1984fef40f60043508c7872d9d7a8748b484a
Bengt Marten Agren <marangisto@me.com>
2019-04-15 13:20:27 +0800

    Basic padding.

--- src/PrettyCpp.hs
+++ src/PrettyCpp.hs
@@ -45,64 +45,32 @@ peripheralStruct findPeripheral Peripheral{..} = unlines $
                       | otherwise = error $ "failed to derive peripheral from " ++ from
 
 registerStructField :: Register -> String
-registerStructField Register{..} = "uint32_t    " <> registerName <> ";" <> pad <> "// " <> offset registerAddressOffset <> unwords (words registerDescription)
+registerStructField Register{..} = mconcat
+    [ "volatile uint32_t    "
+    , registerName
+    , ";"
+    , pad
+    , "// "
+    , offset registerAddressOffset
+    , unwords (words registerDescription)
+    ]
     where pad = replicate (14 - length registerName) ' '
 
 reservedStructField :: Pad -> String
-reservedStructField _ = "// pad"
-
--- [ peripheralStruct findPeripheral p, peripheralFun p ]
-
---pos = OnlyPos nopos (nopos, 0)
+reservedStructField x = mconcat
+    [ "uint32_t             "
+    , "reserved"
+    , show x
+    , ";"
+    ]
 
 removeMe = map (\r@Register{..} -> r {registerName = filter (/='%') registerName})
-
-{-
-peripheralFun :: Peripheral -> Item ()
-peripheralFun Peripheral{..} = Fn [] PublicV (mkIdent $ lowerCase peripheralName)
-    (FnDecl [] (Just (Ptr Mutable (PathTy Nothing (Path False [PathSegment name Nothing ()] ()) ()) ())) False ())
-    Normal NotConst Rust (Generics [] [] (WhereClause [] ()) ())
-    (Block [ NoSemi (Cast [] (Lit [] (Int Dec addr Unsuffixed ()) ())
-             (Ptr Mutable (PathTy Nothing (Path False [PathSegment name Nothing ()] ()) ()) ()) ()) ()
-           ] Normal ()) ()
-    where name = mkIdent peripheralName
-          addr = fromIntegral peripheralBaseAddress
-
-peripheralStruct :: (String -> Maybe Peripheral) -> Peripheral -> Item ()
-peripheralStruct findPeripheral Peripheral{..} = StructItem attributes PublicV (mkIdent peripheralName) variantData generics ()
-    where variantData = StructD (map (either reservedStructField registerStructField) $ padRegisters $ removeMe rs) ()
-          generics = Generics [] [] whereClause ()
-          whereClause = WhereClause [] ()
-          attributes = [ Attribute Outer (Path False [PathSegment "repr" Nothing ()] ()) (delimTree Paren $ IdentTok "C") ()
-                       , SugaredDoc Outer False (' ' : unwords (words peripheralDescription)) ()
-                       ]
-          ([], rs) = partitionEithers $ maybe peripheralRegisters derive peripheralDerivedFrom
-          derive from | Just Peripheral{..} <- findPeripheral from = peripheralRegisters
-                      | otherwise = error $ "failed to derive peripheral from " ++ from
-
--}
 
 padRegisters :: [Register] -> [Either Pad Register]
 padRegisters [] = []
 padRegisters rs = M.elems $ m `M.union` u
     where m = M.fromList [ (registerAddressOffset r, Right r) | r <- rs ]
           u = M.fromList [ (x, Left x) | x <- [ 0, 4..maximum $ M.keys m ] ]
-
-{-
-registerStructField Register{..} = StructField (Just $ mkIdent $ lowerCase registerName) PublicV fieldType attributes ()
-    where fieldType = PathTy Nothing (Path False [PathSegment (mkIdent $ rw registerAccess) (Just (AngleBracketed [] [u32Type] [] ())) ()] ()) ()
-          attributes = [ SugaredDoc Outer False (' ' : offset registerAddressOffset ++ unwords (words registerDescription)) () ]
-
-reservedStructField x = StructField (Just $ mkIdent $ "reserved" ++ hex x) InheritedV u32Type attributes ()
---StructField (Just "reserved0x30") InheritedV (Array (PathTy Nothing (Path False [PathSegment "u32" Nothing ()] ()) ()) (Lit [] (Int Dec 8 Unsuffixed ()) ()) ()) [] ()
-    where attributes = []
-
-u32Type = PathTy Nothing (Path False [PathSegment "u32" Nothing ()] ()) ()
-
-tokenTree x = Tree (Token (Span NoPosition NoPosition) x)
-
-delimTree d x = Tree (Delimited { S.span = Span NoPosition NoPosition, delim = d, tts = tokenTree x })
--}
 
 rw :: Maybe AccessType -> String
 rw (Just AccessType_Read'only) = "RO"
